931b78504c10989e10cc0a8271cfcae2c131d4cb
ashleyAAR <assis.ashley@gmail.com>
2024-06-05 21:22:16 -0300

    added anime bg

--- scripts/home/utils/path.js
+++ scripts/home/utils/path.js
@@ -1,7 +1,4 @@
-const path = require('path');
-
 export function getImageURL(imageName) {
-	const relativePath = `./images/${imageName}`;
-	const absolutePath = path.resolve(relativePath);
-	return absolutePath;
+	const relativePath = `./images/movies/${imageName}`;
+	return relativePath;
 }
